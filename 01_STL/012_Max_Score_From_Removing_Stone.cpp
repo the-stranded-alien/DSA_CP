326d38f161de694e4dfea39fd0d6da0c064b47f1
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-12-04 22:25:21 +0530

    Excercise Problems on STL

---
+++ 01_STL/012_Max_Score_From_Removing_Stone.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int 				long long int
+#define double				long double
+#define F 					first
+#define S 					second
+#define pb 					push_back
+#define pob					pop_back
+#define mp 					make_pair
+#define si 					set <int>
+#define vi 					vector <int>
+#define vs					vector <string>
+#define vb					vector <bool>
+#define pii 				pair <int, int>
+#define vpi         		vector <pii>
+#define vpp         		vector <pair <int, pii> >
+#define vvi					vector <vector<int> >
+#define mii         		map <int, int>
+#define mpi         		map <pii, int>
+#define spi         		set <pii>
+#define nl					"\n"
+#define tb					"\t"
+#define sz(x)				((int) x.size())
+#define all(p)				p.begin(), p.end()
+#define maxQue				priority_queue <int>
+#define minQue				priority_queue <int, vi, greater<int> >
+#define bug(...)			__f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)			for(auto x : a) cout << x << " "; cout << endl
+#define printRange(a,x,y)  	for(int i = x; i < y; i++) cout << a[i] << " "; cout << endl
+#define printRev(a,x,y)		for(int i = x; i > y; i--) cout << a[i] << " "; cout << endl
+#define printPair(p)      	for(auto x : p) cout << x.F << " " << x.S << endl; cout << endl
+#define mod 				1e9 + 7
+
+inline int power (int a, int b) {
+	int x = 1;
+	while (b) {
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args) {
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+// There are three piles of stones of sizes a, b, and c respectivley.
+// Each turn you choose two different non-empty piles, take one stone 
+// from each, and add 1 point to your score. The game stops when there
+// are fewer than two non-empty piles (meaning there are no move moves).
+// Return the maximum score you can get.
+
+int maximumScore(int a, int b, int c) {
+	priority_queue<int> maxHeap;
+	maxHeap.push(a);
+	maxHeap.push(b);
+	maxHeap.push(c);
+	int score = 0;
+	while(maxHeap.size() > 1) {
+		int n1 = maxHeap.top();
+		maxHeap.pop();
+		int n2 = maxHeap.top();
+		maxHeap.pop();
+		score += 1;
+		if(n1 - 1 > 0) maxHeap.push(n1 - 1);
+		if(n2 - 1 > 0) maxHeap.push(n2 - 1);
+	}
+	return score;	
+}
+
+void solve() {
+	// Main Code Goes Here !!
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << maximumScore(a, b, c);
+	return;
+}
+
+int32_t main() {
+	// Fast Input-Output
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	// Input-Output Files
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+	freopen("../error.txt", "w", stderr);
+#endif	
+
+	clock_t startTime = clock();
+
+	int testCases = 1;
+	// cin >> testCases;
+	while(testCases--) solve();
+
+	cerr << "\nRun Time : " << ((double)(clock() - startTime) / CLOCKS_PER_SEC);
+
+	return 0;
+}
