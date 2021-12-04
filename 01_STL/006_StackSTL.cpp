df7e73f285dd9dc245a0e648b892a6b6a9193436
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-12-04 18:54:11 +0530

    Stack & Queue STL

---
+++ 01_STL/006_StackSTL.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+int main() {
+	stack<string> books;
+	books.push("C++");
+	books.push("Java");
+	books.push("Python");
+	books.push("OS");
+
+	while(!books.empty()) {
+		cout << books.top() << " ";
+		books.pop();
+	}
+
+	return 0;
+}
